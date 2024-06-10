f5a03a293d19495c2668c77b7b9b6fb62a6e11ae
cjrojas72 <cjrojas72@gmail.com>
2024-06-09 22:59:38 -0400

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Import the layouts
+import RootLayout from './layouts/root-layout'
+import DashboardLayout from './layouts/dashboard-layout'
+
+// Import the components
+import IndexPage from './routes'
+import SignInPage from './routes/sign-in'
+import SignUpPage from './routes/sign-up'
+import DashboardPage from './routes/dashboard'
+import TransactionsPage from './routes/transactions'
+
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/transactions", element: <TransactionsPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: "dashboard",
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+        ]
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
